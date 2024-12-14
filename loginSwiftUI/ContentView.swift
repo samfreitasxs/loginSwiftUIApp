402e7f12216e616d1511a84241709be61765ca8f
Samuel Freitas <37847692+samfreitasxs@users.noreply.github.com>
2024-12-14 00:14:33 -0300

    include loginSwiftUIApp

--- loginSwiftUI/ContentView.swift
+++ loginSwiftUI/ContentView.swift
@@ -8,12 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var username: String = " "
+    @State var password: String = " "
+    
     var body: some View {
         VStack {
             Text("Welcome Back")
                 .font(.largeTitle)
                 .fontWeight(.black)
                 .padding(.bottom, 42)
+            VStack (spacing: 16.0) {
+                InputFieldView(data: $username, title: "Username")
+                InputFieldView(data: $password, title: "Password")
+            }.padding(.bottom, 16)
             Button(action: {}) {
                 Text ("Sign In")
                 .fontWeight(.heavy)
@@ -21,13 +28,13 @@ struct ContentView: View {
                 .frame(maxWidth: .infinity)
                 .padding()
                 .foregroundColor(.white)
-                .background(LinearGradient(gradient: Gradient(colors: [.pink, .purple]),
+                .background(LinearGradient(gradient: Gradient(colors: [.red, .yellow]),
                     startPoint: .leading, endPoint: .trailing))
-                    .cornerRadius(33)
+                    .cornerRadius(90)
             }
             HStack {
                 Spacer()
-                Text (" Forgotten Password")
+                Text ("Forgotten Password?")
                     .fontWeight(.thin)
                     .foregroundColor(Color.blue)
                     .underline()
